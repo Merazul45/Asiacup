e979927ff5ae9723a1909719f93aa8be613412f8
Merazul45 <77243958+Merazul45@users.noreply.github.com>
2025-02-17 20:04:40 +0600

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// script.js
-document.getElementById('contact-form').addEventListener('submit', function(e) {
-  e.preventDefault(); // Prevent form from submitting
-  alert('Thank you for your message!');
-});
